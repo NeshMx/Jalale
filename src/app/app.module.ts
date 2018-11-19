733aed64fcbbe075e14400d83be717d34647f26c
NeshMx <alex.huerta0424@hotmail.com>
2018-11-18 22:02:38 -0600

    tutorial page ready, working on login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,49 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule, Storage } from '@ionic/storage';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+// import { HomePage } from '../pages/home/home';
+
+// The translate loader needs to know where to load i18n files
+// in Ionic's static asset pipeline.
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    // HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpClientModule,
+    IonicModule.forRoot(MyApp),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    }),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    // HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    // { provide: Settings, useFactory: provideSettings, deps: [Storage] },
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { IonicPage, NavController } from 'ionic-angular';
+
+import { Tab1Root, Tab2Root, Tab3Root } from '../';
+
+/**
+ * Generated class for the TabsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html',
+})
+export class TabsPage {
+
+  tab1Root: any = Tab1Root;
+  tab2Root: any = Tab2Root;
+  tab3Root: any = Tab3Root;
+
+  tab1Title = " ";
+  tab2Title = " ";
+  tab3Title = " ";
+
+  constructor(public navCtrl: NavController, public translateService: TranslateService) {
+    translateService.get(['TAB1_TITLE', 'TAB2_TITLE', 'TAB3_TITLE']).subscribe(values => {
+      this.tab1Title = values['TAB1_TITLE'];
+      this.tab2Title = values['TAB2_TITLE'];
+      this.tab3Title = values['TAB3_TITLE'];
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TabsPage');
+  }
+
+}
