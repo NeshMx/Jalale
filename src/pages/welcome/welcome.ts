c4f0336b8545d89231223d9c3140638061125718
NeshMx <alex.huerta0424@hotmail.com>
2018-11-21 23:16:43 -0600

    mobile login implemented (not working in browser), added android platform and config files

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -3,7 +3,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { IonicPage, NavController } from 'ionic-angular';
 
 import { AngularFireAuth } from '@angular/fire/auth';
-import firebase  from 'firebase';
+import firebase from 'firebase';
 import { MainPage } from '../';
 
 /**
@@ -27,49 +27,94 @@ export class WelcomePage {
     loggedIn: false
   }
 
-
-  // Our translated text strings
-  private loginSuccessString: string;
-
-  constructor(private fire: AngularFireAuth, 
-    public navCtrl: NavController, 
+  constructor(private fire: AngularFireAuth,
+    public navCtrl: NavController,
     public translateService: TranslateService) {
 
-    this.translateService.get('LOGIN_SUCCESS').subscribe((value) => {
-      this.loginSuccessString = value;
-    })
   }
+
+  // loginWithFacebook() {
+  //   this.fire.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+  //   .then( res => {
+  //     console.log(res);
+  //     this.provider.loggedIn = true;
+  //     this.provider.name = res.user.displayName;
+  //     this.provider.profilePicture = res.user.photoURL;
+  //     this.provider.email = res.user.email;
+  //     window.localStorage.setItem('fbLoggedIn', this.provider.loggedIn.toString());
+  //     window.localStorage.setItem('fbName', this.provider.name);
+  //     window.localStorage.setItem('fbProfilePicture', this.provider.profilePicture);
+  //     window.localStorage.setItem('fbEmail', this.provider.email);
+  //     this.navCtrl.push(MainPage);
+  //   });
+  // }
 
   loginWithFacebook() {
-    this.fire.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
-    .then( res => {
-      console.log(res);
-      this.provider.loggedIn = true;
-      this.provider.name = res.user.displayName;
-      this.provider.profilePicture = res.user.photoURL;
-      this.provider.email = res.user.email;
-      window.localStorage.setItem('fbLoggedIn', this.provider.loggedIn.toString());
-      window.localStorage.setItem('fbName', this.provider.name);
-      window.localStorage.setItem('fbProfilePicture', this.provider.profilePicture);
-      window.localStorage.setItem('fbEmail', this.provider.email);
-      this.navCtrl.push(MainPage);
-    });
+    this.fire.auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())
+      .then(() => {
+        this.fire.auth.getRedirectResult().then(res => {
+          console.log(res);
+          this.provider.loggedIn = true;
+          this.provider.name = res.user.displayName;
+          this.provider.profilePicture = res.user.photoURL;
+          this.provider.email = res.user.email;
+          window.localStorage.setItem('fbLoggedIn', this.provider.loggedIn.toString());
+          window.localStorage.setItem('fbName', this.provider.name);
+          window.localStorage.setItem('fbProfilePicture', this.provider.profilePicture);
+          window.localStorage.setItem('fbEmail', this.provider.email);
+          this.navCtrl.push(MainPage);
+        }).catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+        });
+      }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
   }
 
+  // loginWithGoogle() {
+  //   this.fire.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+  //     .then(res => {
+  //       console.log(res);
+  //       this.provider.loggedIn = true;
+  //       this.provider.name = res.user.displayName;
+  //       this.provider.profilePicture = res.user.photoURL;
+  //       this.provider.email = res.user.email;
+  //       window.localStorage.setItem('goLoggedIn', this.provider.loggedIn.toString());
+  //       window.localStorage.setItem('goName', this.provider.name);
+  //       window.localStorage.setItem('goProfilePicture', this.provider.profilePicture);
+  //       window.localStorage.setItem('goEmail', this.provider.email);
+  //       this.navCtrl.push(MainPage);
+  //     });
+  // }
+
   loginWithGoogle() {
-    this.fire.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
-    .then( res => {
-      console.log(res);
-      this.provider.loggedIn = true;
-      this.provider.name = res.user.displayName;
-      this.provider.profilePicture = res.user.photoURL;
-      this.provider.email = res.user.email;
-      window.localStorage.setItem('goLoggedIn', this.provider.loggedIn.toString());
-      window.localStorage.setItem('goName', this.provider.name);
-      window.localStorage.setItem('goProfilePicture', this.provider.profilePicture);
-      window.localStorage.setItem('goEmail', this.provider.email);
-      this.navCtrl.push(MainPage);
-    });
+    this.fire.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+      .then(() => {
+        this.fire.auth.getRedirectResult().then(res => {
+          console.log(res);
+          this.provider.loggedIn = true;
+          this.provider.name = res.user.displayName;
+          this.provider.profilePicture = res.user.photoURL;
+          this.provider.email = res.user.email;
+          window.localStorage.setItem('goLoggedIn', this.provider.loggedIn.toString());
+          window.localStorage.setItem('goName', this.provider.name);
+          window.localStorage.setItem('goProfilePicture', this.provider.profilePicture);
+          window.localStorage.setItem('goEmail', this.provider.email);
+          this.navCtrl.push(MainPage);
+        }).catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+        });
+      }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
   }
 
 }
